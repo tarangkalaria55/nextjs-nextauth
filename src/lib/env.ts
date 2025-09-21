eafe58c49ff437d5bfe6bc3331ec296f06d4a5b2
Tarang Kalariya <tarangkalaria55@gmail.com>
2025-09-21 23:10:19 +0530

    feat: add environment variable management with zod validation
    - Introduced a new env.ts file to manage environment variables using @t3-oss/env-nextjs.
    - Defined server and client environment variables with validation rules.
    - Implemented error handling for validation failures and invalid access attempts.
    - Added a .gitkeep file in the drizzle directory to ensure it is tracked in version control.

---
+++ src/lib/env.ts
@@ -0,0 +1,26 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_PUBLISHABLE_KEY: z.string().min(1),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
+  },
+  emptyStringAsUndefined: false,
+  // Called when the schema validation fails.
+  onValidationError: (issues) => {
+    console.error('❌ Invalid environment variables:', issues);
+    throw new Error('Invalid environment variables');
+  },
+  // Called when server variables are accessed on the client.
+  onInvalidAccess: () => {
+    throw new Error(
+      '❌ Attempted to access a server-side environment variable on the client'
+    );
+  },
+});
